81c3dbcd401209b42768edad30510df10d912d01
xaviernadal <xavinadal14@gmail.com>
2022-07-06 14:46:17 +0200

    Corrected the tabs error

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: '',
   appName: 'films-app',
   webDir: 'www',
   bundledWebRuntime: false
